eb9f6b5de5d6e8c1fe1ccf7accfceb5c6f195ae1
Amirhossein Abolhassani <aa86868699@gmail.com>
2026-02-18 11:56:09 +0330

    feat(ui): add Button struct + hit-test API

---
+++ ui_button.h
@@ -0,0 +1,32 @@
+#ifndef FOP_PROJECT_G66_UI_BUTTON_H
+#define FOP_PROJECT_G66_UI_BUTTON_H
+
+#pragma once
+#include <SDL2/SDL.h>
+#include <functional>
+
+namespace ui {
+
+    struct button {
+        SDL_Rect rect { 0 , 0 , 0 , 0 } ;
+        bool enabled { true } ;
+
+        // Called when a click happens inside rect (and enabled == true)
+        std :: function <void()> on_click {} ;
+    };
+
+
+    // Basic hit test
+    bool button_hit ( const button &b , int x , int y ) ;
+
+
+    // Triggers on_click if hit (return true if consumed)
+    bool button_handle_click ( const button &b , int x , int y ) ;
+
+
+    // Minimal visual
+    void button_draw ( SDL_Renderer *r , const button &b , SDL_Color fill , SDL_Color border ) ;
+
+}
+
+#endif //FOP_PROJECT_G66_UI_BUTTON_H
