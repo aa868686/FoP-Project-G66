b5a774bbad5ec2b77ba56902c14b5564e4ccceff
Amirhossein Abolhassani <aa86868699@gmail.com>
2026-02-20 23:37:23 +0330

    feat(sound): play default sound on sprite click in stage; implement play_sound block in interpreter with sound_manager pointer; wire sound_manager to interpreter in runtime

--- runtime.cpp
+++ runtime.cpp
@@ -548,6 +548,7 @@ namespace app {
         if (tex) {
             gfx::sprite_add_costume(s, tex, w, h, "default");
         }
+
         gfx::sprite_set_position(s, st.lay.stage.w * 0.5f, st.lay.stage.h * 0.5f ) ;
         gfx::sprite_manager_add(st.sprite_mgr, s);
         gfx::sprite_manager_select(st.sprite_mgr, 0);
